d6a1d1d28f40aa281ab0de9b171c5e4c4ab193b3
lucas_abmael <124467012+lucas22022008@users.noreply.github.com>
2023-05-01 13:05:39 -0300

    mostrar e ocultar a senha

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const container = document.querySelector(".content");
 const icon = document.querySelector(".icon");
 
+const password = document.getElementById("senha");
+const iconPassword = document.getElementById("icon");
+
 icon.addEventListener("click", () => {
     container.classList.toggle('dark');
 })
+
+function showHide() {
+    if(password.type === 'password'){
+        password.setAttribute('type', 'text');
+        iconPassword.classList.add('hide');
+    }
+    else{
+        password.setAttribute('type', 'password');
+        iconPassword.classList.remove('hide');
+    }
+}
